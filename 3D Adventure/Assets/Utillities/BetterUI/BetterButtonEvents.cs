7b91fe860b085a2790fa95b49e8fc10389b95c1e
Wokarol <wojtek.karolczak20@gmail.com>
2018-07-18 22:03:13 +0200

    Setted up project

---
+++ 3D Adventure/Assets/Utillities/BetterUI/BetterButtonEvents.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Wokarol {
+	[AddComponentMenu("UI/BetterButtonEvents")]
+	public class BetterButtonEvents : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler {
+
+		[SerializeField] UnityEvent OnDown;
+		[SerializeField] UnityEvent OnUp;
+		[SerializeField] UnityEvent OnExit;
+		[SerializeField] UnityEvent OnEnter;
+
+		public void OnPointerDown (PointerEventData eventData) {
+			OnDown.Invoke();
+		}
+
+		public void OnPointerUp (PointerEventData eventData) {
+			OnUp.Invoke();
+		}
+
+		public void OnPointerExit (PointerEventData eventData)
+		{
+			OnExit.Invoke();
+		}
+
+		public void OnPointerEnter (PointerEventData eventData)
+		{
+			OnEnter.Invoke();
+		}
+
+	}
+}
