ebcca8211b9ee97ed14b8564036f6918b8f3bc30
Wokarol <wojtek.karolczak20@gmail.com>
2018-07-30 16:45:57 +0200

    Added some light and base flares;

---
+++ 3D Adventure/Assets/FlareIntensity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wokarol
+{
+	public class FlareIntensity : MonoBehaviour 
+	{
+		// Variables
+		[Range(0,1)] [SerializeField] private float intensity;
+
+		public float Intensity {
+			get {
+				return intensity;
+			}
+
+			set {
+				intensity = value;
+			}
+		}
+		// Functions
+
+	}
+}
--- 3D Adventure/Assets/Scripts/DebugToText.cs
+++ 3D Adventure/Assets/Scripts/DebugToText.cs
@@ -8,8 +8,13 @@ namespace Wokarol
 	[RequireComponent(typeof(TextMeshProUGUI))]
 	public class DebugToText : MonoBehaviour 
 	{
+		const string frameCountTag = "{frame}";
+		const string logTextTag = "{log}";
+
 		// Variables
-		[SerializeField] string lookUpString;
+		[SerializeField] string lookUpString = ":OnScreen:";
+		[TextArea(1, 3)]
+		[SerializeField] string messageTemplate = "{frame}: {log}";
 
 		TextMeshProUGUI textMesh;
 		// Functions
@@ -27,6 +32,21 @@ namespace Wokarol
 			}
 		}
 
+		string ProcessText(string text, string logString, int frame)
+		{
+			while (text.Contains(logTextTag)) {
+				text = text.Replace(logTextTag, logString);
+			}
+			while (text.Contains(frameCountTag)) {
+				text = text.Replace(frameCountTag, frame.ToString());
+			}
+			//while (text.Contains("\\t")) {
+			//	text = text.Replace(frameCountTag, "\t");
+			//}
+
+			return text;
+		}
+
 		void OnEnable ()
 		{
 			Application.logMessageReceived += HandleLog;
@@ -35,17 +55,32 @@ namespace Wokarol
 		{
 			Application.logMessageReceived -= HandleLog;
 		}
-		void HandleLog (string logString, string stackTrace, LogType type)
+		void AddLog(string logString, int frame)
 		{
 			var text = textMesh.text;
 			if (logString.Contains(lookUpString)) {
 				string textToShow = logString.Replace(lookUpString, "");
-				textToShow = Time.frameCount + ": " + textToShow;
-				text += textToShow;
+				text += ProcessText(messageTemplate, textToShow, frame);
 				text += "\n";
 				textMesh.text = text;
 			}
 		}
+		void HandleLog (string logString, string stackTrace, LogType type)
+		{
+			AddLog(logString, Time.frameCount);
+		}
 
+		private void OnValidate ()
+		{
+			if(textMesh == null) {
+				textMesh = GetComponent<TextMeshProUGUI>();
+			}
+			textMesh.text = "";
+
+			AddLog(lookUpString + "Test log", 45);
+			AddLog(lookUpString + "Another thing", 456);
+			AddLog(lookUpString + "Hell yeah", 500);
+			AddLog(lookUpString + "Testing is fun", 1024);
+		}
 	}
 }
