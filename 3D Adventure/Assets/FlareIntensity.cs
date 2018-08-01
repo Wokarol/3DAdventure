e5a75b32506b81561eb0ea032b2afb815178200d
Wokarol <wojtek.karolczak20@gmail.com>
2018-08-01 13:23:45 +0200

    Added intensity on flares; Changed bloom a little; Deleted Player-Flares collision; Temporary deleted ProGrids;

--- 3D Adventure/Assets/FlareIntensity.cs
+++ 3D Adventure/Assets/FlareIntensity.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 namespace Wokarol
 {
+	[RequireComponent(typeof(Light), typeof(MeshRenderer))]
 	public class FlareIntensity : MonoBehaviour 
 	{
 		// Variables
-		[Range(0,1)] [SerializeField] private float intensity;
-
+		[Range(0,1)] [SerializeField] private float intensity = 1;
 		public float Intensity {
 			get {
 				return intensity;
@@ -18,7 +18,41 @@ namespace Wokarol
 				intensity = value;
 			}
 		}
+
+		[SerializeField] int lightMaterialIndex = 0;
+
+		new Light light;
+		float lightMaxIntensity;
+
+		MeshRenderer meshRenderer;
+		MaterialPropertyBlock materialPropertyBlock;
+		Color maxMaterialColor;
 		// Functions
+
+		private void Start ()
+		{
+			materialPropertyBlock = new MaterialPropertyBlock();
+
+			light = GetComponent<Light>();
+			lightMaxIntensity = light.intensity;
+
+			meshRenderer = GetComponent<MeshRenderer>();
+			maxMaterialColor = meshRenderer.sharedMaterials[lightMaterialIndex].GetColor("_EmissionColor");
+		}
+
+		private void Update ()
+		{
+			SetIntensity(intensity);
+		}
+
+		void SetIntensity (float _intensity)
+		{
+			light.intensity = Mathf.Lerp(0, lightMaxIntensity, _intensity);
+			meshRenderer.GetPropertyBlock(materialPropertyBlock, lightMaterialIndex);
+			materialPropertyBlock.SetColor("_EmissionColor", Color.Lerp(Color.black, maxMaterialColor, _intensity));
+			meshRenderer.SetPropertyBlock(materialPropertyBlock, lightMaterialIndex);
+			
+		}
 
 	}
 }
---
+++ 3D Adventure/Assets/RandomFlareIntensity.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Wokarol
+{
+	[RequireComponent(typeof(FlareIntensity))]
+	public class RandomFlareIntensity : MonoBehaviour 
+	{
+		// Variables
+		FlareIntensity flareIntensity;
+		[SerializeField] FloatVariableReference perlinNoiseTest;
+		// Functions
+		private void Awake ()
+		{
+			flareIntensity = GetComponent<FlareIntensity>();
+		}
+		private void Update ()
+		{
+			perlinNoiseTest.Value = Mathf.PerlinNoise(0, 0);
+		}
+	}
+}
